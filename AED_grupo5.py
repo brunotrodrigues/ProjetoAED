fd576f4a19ba2e3c978b04382e03611b8e22a2c1
Bruno Rodrigues <72968675+mrbonur7@users.noreply.github.com>
2021-01-16 15:11:49 +0000

    Add files via upload

--- AED_grupo5.py
+++ AED_grupo5.py
@@ -4,6 +4,7 @@ from tkinter import filedialog
 from tkinter import messagebox
 import os
 import re
+import datetime
     
 def registar():
     global ecra_registar 
@@ -96,13 +97,14 @@ def login():
     
     window.destroy()
 
-    global ecra_login, username_ver, password_ver, username_log, password_log
+    global ecra_login, username_ver, password_ver, username_log, password_log #, perfil_log, perfil_ver
     ecra_login = Tk()
     ecra_login.title("Login")
     ecra_login.geometry("300x250")
 
     username_ver = StringVar()
     password_ver = StringVar()
+    #perfil_ver = StringVar()
 
     username_log_label = Label(ecra_login, text="Username:")
     username_log_label.place(x=50, y=50)
@@ -112,14 +114,19 @@ def login():
     password_log_label.place(x=50, y=100)
     password_log = Entry(ecra_login, textvariable=password_ver, show="*")
     password_log.place(x=120, y=100)
-    Button(ecra_login, text="Login", background="skyblue1", command = login_ver).place(x=200, y=150)
 
+    btn1 = Button(ecra_login, text="Login", background="skyblue1", command = login_ver)
+    btn1.place(x=200, y=200)
+    
 def login_ver():
 
     user1 = username_ver.get()
     pass1 = password_ver.get()
+    #perfil1 = perfil_ver.get()
     username_log.delete(0, END)
     password_log.delete(0, END)
+    #perfil_log.delete(0, END)
+
  
     file = open("utilizadores.txt", "r")
     lista_files = file.read()
@@ -156,17 +163,22 @@ def main_account_screen():
     window.title("Login")
     window.configure(background='white')
 
-    Button(text="Login", height="2", width="30", background="skyblue1", command = login).place(x=50, y=50)
-    Button(text="Registar", height="2", width="30", background="skyblue1", command = registar).place(x=50, y=100)
+    btnlogin = Button(text="Login", height="2", width="30", background="skyblue1", command = login)
+    btnlogin.place(x=50, y=50)
+    btnregistar = Button(text="Registar", height="2", width="30", background="skyblue1", command = registar)
+    btnregistar.place(x=50, y=100)
 
     window.mainloop()
 
 def tarefas_screen():
+ 
+    global ecra_tarefas
+
     ecra_tarefas=Tk()
     ecra_tarefas.title("Tarefas")
     ecra_tarefas.geometry("1000x1000")
     ecra_tarefas.resizable(1,0)
-    
+
     #Implementar menu
     barra_menu = Menu(ecra_tarefas)
     
@@ -176,8 +188,8 @@ def tarefas_screen():
 
     #Menu das tarefas
     tarefaMenu = Menu(barra_menu, tearoff=0)
-    tarefaMenu.add_command(label = "Nova tarefa")
-    tarefaMenu.add_command(label = "Editar tarefa")
+    tarefaMenu.add_command(label = "Nova tarefa", command = ntarefas)
+    tarefaMenu.add_command(label = "Editar tarefa", command = etarefas)
     barra_menu.add_cascade(label = "Tarefas", menu = tarefaMenu)
     
     #Menu de produtividade
@@ -189,7 +201,7 @@ def tarefas_screen():
     contaMenu.add_command(label = "Editar conta")
     contaMenu.add_command(label = "Abrir")
     contaMenu.add_separator()
-    contaMenu.add_command(label = "Log out")
+    contaMenu.add_command(label = "Log out", command = logout)
     barra_menu.add_cascade(label = "Conta", menu = contaMenu)
     
     #frame onde vai aparecer as tarefas
@@ -201,18 +213,21 @@ def tarefas_screen():
     frame_filtros.place(x=50, y=20)
 
     #Filtros de pesquisa
-    val = IntVar(0)
+    hoje = StringVar()
+    categoria = IntVar()
+    ptarefas = IntVar()
+    estadotarefas = IntVar()
 
-    check1 = Checkbutton(frame_filtros, text = "Hoje", variable = val)
+    check1 = Checkbutton(frame_filtros, text = "Hoje",variable = hoje)
     check1.place(x=15, y=15)
 
-    check2 = Checkbutton(frame_filtros, text = "Por categoria", variable = val)
+    check2 = Checkbutton(frame_filtros, text = "Por categoria", variable = categoria)
     check2.place(x=145, y=15)
 
-    check3 = Checkbutton(frame_filtros, text = "Próximas tarefas", variable = val)
+    check3 = Checkbutton(frame_filtros, text = "Próximas tarefas", variable = ptarefas)
     check3.place(x=15, y=45)
 
-    check4 = Checkbutton(frame_filtros, text = "Estado das tarefas", variable = val)
+    check4 = Checkbutton(frame_filtros, text = "Estado das tarefas", variable = estadotarefas)
     check4.place(x=145, y=45)
 
     #Frame filtro por categoria
@@ -220,18 +235,21 @@ def tarefas_screen():
     frame_categorias.place(x=50, y=135)
 
     #Filtros de categoria
-    val1 = IntVar(0)
+    important = IntVar()
+    trabalho = IntVar()
+    pessoal = IntVar()
+    outros = IntVar()
 
-    check5 = Checkbutton(frame_categorias, text = "Importante", variable = val1)
+    check5 = Checkbutton(frame_categorias, text = "Importante", variable = important)
     check5.place(x=15, y=15)
 
-    check6 = Checkbutton(frame_categorias, text = "Trabalho", variable = val1)
+    check6 = Checkbutton(frame_categorias, text = "Trabalho", variable = trabalho)
     check6.place(x=145, y=15)
 
-    check7 = Checkbutton(frame_categorias, text = "Pessoal", variable = val1)
+    check7 = Checkbutton(frame_categorias, text = "Pessoal", variable = pessoal)
     check7.place(x=15, y=45)
 
-    check8 = Checkbutton(frame_categorias, text = "Outros", variable = val1)
+    check8 = Checkbutton(frame_categorias, text = "Outros", variable = outros)
     check8.place(x=145, y=45)
 
     #Frame filtro Estado de tarefas
@@ -239,15 +257,17 @@ def tarefas_screen():
     frame_estado.place(x=50,y=250)
 
     #Filtros de Estado de tarefas
-    val2 = IntVar(0)
+    concluidas = IntVar()
+    desenvolvimento = IntVar()
+    atraso = IntVar()
 
-    check9 = Checkbutton(frame_estado,text = "Concluídas", variable = val2)
+    check9 = Checkbutton(frame_estado,text = "Concluídas", variable = concluidas)
     check9.place(x=15, y=15)
 
-    check10 = Checkbutton(frame_estado, text = "Em desenvolvimento", variable = val2)
+    check10 = Checkbutton(frame_estado, text = "Em desenvolvimento", variable = desenvolvimento)
     check10.place(x=145, y=15)
 
-    check11 = Checkbutton(frame_estado, text = "Em atraso", variable = val2)
+    check11 = Checkbutton(frame_estado, text = "Em atraso", variable = atraso)
     check11.place(x=15, y=45)
 
     #Frames de filtros de próximas tarefas
@@ -255,19 +275,86 @@ def tarefas_screen():
     frame_proximas.place(x=50,y=365)
 
     #Filtros de Próximas Tarefas
-    val3 = IntVar(0)
+    amanha = IntVar()
+    esemana = IntVar()
+    psemana = IntVar()
+    pmes = IntVar()
 
-    check12 = Checkbutton(frame_proximas, text = "Amanhã", variable = val3)
+    check12 = Checkbutton(frame_proximas, text = "Amanhã", variable = amanha)
     check12.place(x=15, y=15)
 
-    check13 = Checkbutton(frame_proximas, text = "Esta semana", variable = val3)
+    check13 = Checkbutton(frame_proximas, text = "Esta semana", variable = esemana)
     check13.place(x=145, y=15)
 
-    check14 = Checkbutton(frame_proximas, text = "Próxima semana", variable = val3)
+    check14 = Checkbutton(frame_proximas, text = "Próxima semana", variable = psemana)
     check14.place(x=15, y=45)
 
-    check15 = Checkbutton(frame_proximas, text = "Próximo mês", variable = val3)
+    check15 = Checkbutton(frame_proximas, text = "Próximo mês", variable = pmes)
     check15.place(x=145, y=45)
+
     ecra_tarefas.mainloop()
+
+def logout():
+    ecra_tarefas.destroy()
+
+def ntarefas():
+    ecra_tarefas.withdraw()
+    
+    global ecra_ntarefas, newtask_des, ntdes, newtask_cat, ntcat, categoria, einicial, newtask_ei
+    categoria = StringVar()
+    einicial = StringVar()
+
+    ecra_ntarefas=Tk()
+    ecra_ntarefas.title("Nova Tarefas")
+    ecra_ntarefas.geometry("1000x1000")
+    ecra_ntarefas.resizable(1,0)
+
+    ntdes = StringVar()
+    ntcat = StringVar()
+
+    newtask_label_des = Label(ecra_ntarefas, text="Designação:")
+    newtask_label_des.place(x=50, y=50)
+    newtask_des = Entry(ecra_ntarefas, textvariable=ntdes)
+    newtask_des.place(x=120, y=50)
+
+    lista1 = ["Importante", "Trabalho", "Outros", "Pessoal"]
+
+    newtask_label_cat = Label(ecra_ntarefas, text="Categoria:")
+    newtask_label_cat.place(x=50, y=100)
+    newtask_cat = Combobox(ecra_ntarefas, state="readonly", values=lista1, textvariable=categoria)
+    newtask_cat.place(x=120, y=100)
+
+    lista2 = ["Não realizado", "Em desenvolvimento", "Finalizado"]
+
+    newtask_label_ei = Label(ecra_ntarefas, text="Estado inicial:")
+    newtask_label_ei.place(x=50, y=150)
+    newtask_ei = Combobox(ecra_ntarefas, state="readonly", values=lista2, textvariable=einicial)
+    newtask_ei.place(x=130, y=150)
+
+def etarefas():
+    
+    ecra_tarefas.withdraw()  
+
+    ecra_etarefas=Tk()
+    ecra_etarefas.title("Editar Tarefas")
+    ecra_etarefas.geometry("1000x1000")
+    ecra_etarefas.resizable(1,0)
+
+def notifications():
+    
+    ecra_popup=Tk()
+    ecra_popup.title("Notificações")
+    ecra_popup.geometry("200x200")
+    ecra_popup.resizable(1,0)
+
+def gerir():
+    
+    #só admins podem aceder a está pagina
+
+    ecra_gerir=Tk()
+    ecra_gerir.title("Gerir")
+    ecra_gerir.geometry("1000x1000")
+    ecra_gerir.resizable(1,0)
+
 
 main_account_screen() 
